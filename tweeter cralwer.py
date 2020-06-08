3dc7dbf609938e2c06c4c6b980ce6fd5b116bb95
Alex Chen <33658542+OuOnya@users.noreply.github.com>
2020-06-08 18:23:02 +0800

    crawl for each day
    crawl for each day

--- tweeter cralwer.py
+++ tweeter cralwer.py
@@ -1,4 +1,5 @@
 import os
+import math
 import time
 import json
 import codecs
@@ -16,10 +17,10 @@ from argparse import Namespace
 
 args = Namespace(
     keyword="COVID-19 (#COVID-19) (@@realDonaldTrump)",
-    since="2020-05-14",
-    until="2020-05-15",
+    since="2020-03-01",
+    until="2020-06-01",
     limit=0,
-    saved_file="COVID19 realDonaldTrump 0514_0515.csv",
+    saved_file="COVID19 realDonaldTrump 0301_0531.csv",
 )
 
 
@@ -48,29 +49,51 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield (start_date + dt.timedelta(n), start_date + dt.timedelta(n + 1))
+
+
 if __name__ == '__main__':
-    # Save the retrieved tweets to file:
-    start = time.time()
-    tweets = query_tweets(query=args.keyword,
-                          limit=args.limit,
-                          begindate=datetime.strptime(
-                              args.since, '%Y-%m-%d').date(),
-                          enddate=datetime.strptime(
-                              args.until, '%Y-%m-%d').date(),
-                          poolsize=20,
-                          lang='en')
-    json_tweets = json.dumps(tweets, cls=JSONEncoder)
-    pd_tweets = pd.read_json(json_tweets).drop_duplicates()
-    pd_tweets.to_csv(args.saved_file, index=False, encoding='utf-8')
-    print(f'{time.time()-start} s')
+    start_date = datetime.strptime(args.since, '%Y-%m-%d').date()
+    end_date = datetime.strptime(args.until, '%Y-%m-%d').date()
+    for begindate, enddate in daterange(start_date, end_date):
+        print(begindate.strftime("%Y-%m-%d"), enddate.strftime("%Y-%m-%d"))
 
-    # start = time.time()
-    # os.system(
-    #     f'twitterscraper "{args.keyword}" -bd {args.since} -ed {args.until} --lang en -o tweets.json -ow')
-    # print(time.time() - start)
+        start = time.time()
+        # Retrieved tweets to file:
+        tweets = query_tweets(query=args.keyword,
+                              limit=args.limit,
+                              begindate=begindate,
+                              enddate=enddate,
+                              poolsize=20,
+                              lang='en')
+        print(f'{time.time()-start} s')
 
-    pd_tweets = pd.read_csv(args.saved_file)
-    print(pd_tweets)
-    unique_user_count = pd_tweets.groupby(
-        pd_tweets.user_id.tolist(), as_index=False).size()
-    print(unique_user_count)
+        if len(tweets) != 0:
+            json_tweets = json.dumps(tweets, cls=JSONEncoder)
+            pd_tweets = pd.read_json(json_tweets).drop_duplicates()
+            unique_user = pd_tweets.groupby(
+                pd_tweets.user_id.tolist(), as_index=False).size()
+
+            tweets_count = len(pd_tweets)
+            unique_user_count = len(unique_user)
+            influence_score = (tweets_count / (unique_user_count + 1)) * \
+                math.log10(tweets_count + 1)
+        else:
+            tweets_count = 0
+            unique_user_count = 0
+            influence_score = 0
+
+        data = {
+            'date': [begindate.strftime("%Y-%m-%d")],
+            'tweets_count': [tweets_count],
+            'unique_user_count': [unique_user_count],
+            'influence_score': [influence_score],
+        }
+        results = pd.DataFrame.from_dict(data)
+        results.to_csv(
+            args.saved_file, mode='a',
+            header=not os.path.exists(args.saved_file),
+            index=False, encoding='utf-8'
+        )
